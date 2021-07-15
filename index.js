4e81806b0c6a73da09eb6853a0978baf050bcb90
Rorisang07 <rorisang050@gmail.com>
2021-07-15 15:01:08 +0200

    updted website

---
+++ index.js
@@ -0,0 +1,7 @@
+var modal = document.getElementById('login');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
